eaeb760a8c5a915a662a458b17f78a8548b00fce
Antony JACQUES <antony.jacques-ext@gl-events.com>
2023-12-09 13:43:45 +0100

    103 Signup & Login forms

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <SignupFrom />
+    <LoginForm />
   </div>
 </template>
 
 <script>
+import SignupFrom from "../components/SignupForm.vue";
+import LoginForm from "../components/LoginForm.vue";
 export default {
-
-}
+  components: { SignupFrom, LoginForm },
+};
 </script>
 
 <style>
-.welcome {
+  .welcome {
     text-align: center;
     padding: 20px 0;
-}
-
+  }
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
 </style>
