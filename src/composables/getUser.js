8c7f2f19373e1fe126d4c4f0bea13769d857035c
Antony JACQUES <antony.jacques-ext@gl-events.com>
2023-12-11 19:02:36 +0100

    get the current user

---
+++ src/composables/getUser.js
@@ -0,0 +1,19 @@
+import { projectAuth } from "@/firebase/config";
+import { ref } from "vue";
+
+const error = ref(null);
+
+const user = ref(projectAuth.currentUser);
+
+// this f fire everytime the user login or logout
+// if the user is logged in, user's data are in _user
+projectAuth.onAuthStateChanged((_user) => {
+  console.log("user state change. Current user is", _user);
+  user.value = _user;
+});
+
+const getUser = () => {
+  return { user };
+};
+
+export default getUser;
