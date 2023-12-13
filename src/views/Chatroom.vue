d0758fb7c5f2db6dd2e070eb37f05b38054db35e
Antony JACQUES <antony.jacques-ext@gl-events.com>
2023-12-13 21:28:13 +0100

    waiting firebase Auth to init

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="container">
-    <Navbar/>
+    <Navbar />
   </div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
+//get the current user using getUser
+// watch the current user for changes
+// redirect the user to the welcome page when they are logged out
+import Navbar from "../components/Navbar.vue";
+import getUser from "@/composables/getUser";
+import { projectAuth } from "@/firebase/config";
+import router from "@/router";
 export default {
-  components: { Navbar }
+  components: { Navbar },
+  setup() {
+    const { user } = getUser();
 
-}
+    projectAuth.onAuthStateChanged((_user) => {
+      user.value = _user;
+      if (!user.value) {
+        router.push({ name: "welcome" });
+      }
+    });
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
