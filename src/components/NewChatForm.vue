111b1f037a182ca57dc1e637401bc70c440f9f4e
Antony JACQUES <antony.jacques-ext@gl-events.com>
2023-12-13 22:26:26 +0100

    add new message

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -5,6 +5,7 @@
       v-model="message"
       @keypress.enter.prevent="handleSubmit"
     ></textarea>
+    <div class="error">{{ error }}</div>
   </form>
 </template>
 
@@ -12,10 +13,13 @@
 import getUser from "@/composables/getUser";
 import { ref } from "vue";
 import { timestamp } from "@/firebase/config";
+import useCollection from "@/composables/useCollection";
+
 export default {
   setup() {
     const message = ref("");
     const { user } = getUser();
+    const { error, addDoc } = useCollection("messages");
 
     const handleSubmit = async () => {
       const chat = {
@@ -24,26 +28,29 @@ export default {
         createdAt: timestamp(),
       };
       console.log(chat);
-      message.value = "";
+      await addDoc(chat);
+      if(!error.value){
+        message.value = "";
+      }
     };
-    return { message, handleSubmit };
+    return { message, handleSubmit, error };
   },
 };
 </script>
 
 <style scoped>
-  form {
-    margin: 10px;
-  }
-  textarea {
-    width: 100%;
-    max-width: 100%;
-    margin-bottom: 6px;
-    padding: 10px;
-    box-sizing: border-box;
-    border: 0;
-    border-radius: 20px;
-    font-family: inherit;
-    outline: none;
-  }
+form {
+  margin: 10px;
+}
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
 </style>
